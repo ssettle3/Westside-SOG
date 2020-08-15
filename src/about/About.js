02c835d5b7567b4b91c85427cc7f902ed0259bc6
Stephen Settle <ssettle3@gmail.com>
2020-08-15 13:07:44 -0400

    Tweak styles for About page

--- src/about/About.js
+++ src/about/About.js
@@ -4,9 +4,13 @@ import Tmdb from "../images/tmdb-credit.svg";
 import { Space } from "../space/Space";
 
 const Container = styled.div`
-  padding: 20px;
+  padding: 100px;
   display: flex;
   flex-direction: column;
+
+  @media only screen and (max-width: 600px) {
+    padding: 20px;
+  }
 `;
 
 const Bold = styled.div`
@@ -19,31 +23,37 @@ const Answer = styled.div`
   margin-bottom: 30px;
 `;
 
-export const About = () => {
-  return (
-    <Container>
-      <Bold>What is this?</Bold>
-      <Answer>
-        Screen on the Green - Westside is the simple app created for the cool
-        people on Ellen Street to help them recommend and select movies to watch
-        :)
-      </Answer>
-      <Bold>What do I do?</Bold>
-      <Answer>
-        You can search for a movie by name or explore the different genre this
-        app divides up for you! When you see a movie you want to recommend
-        simply tap or click on the thumbs up icon in the upper right corner of
-        the movie. If a movie has already been recommended by you the thumbs up
-        icon will be filled in. You can un recommend if you would like as well.
-      </Answer>
-      <Bold>Who made this?</Bold>
-      <Answer>A really cool guy named Stephen Settle.</Answer>
-      <Bold>Credit</Bold>
-      <Answer>
-        All movie data as it pertains to this app came from
-        <Space pad="10px" />
-        <img alt="tmdb" src={Tmdb} />
-      </Answer>
-    </Container>
-  );
-};
+const Image = styled.img`
+  width: 200px;
+
+  @media only screen and (max-width: 600px) {
+    width: 100%;
+  }
+`;
+
+export const About = () => (
+  <Container>
+    <Bold>What is this?</Bold>
+    <Answer>
+      Screen on the Green - Westside is the simple app created for the cool
+      people on Ellen Street to help them recommend and select movies to watch
+      :)
+    </Answer>
+    <Bold>What do I do?</Bold>
+    <Answer>
+      You can search for a movie by name or explore the different genre this app
+      divides up for you! When you see a movie you want to recommend simply tap
+      or click on the thumbs up icon in the upper right corner of the movie. If
+      a movie has already been recommended by you the thumbs up icon will be
+      filled in. You can un recommend if you would like as well.
+    </Answer>
+    <Bold>Who made this?</Bold>
+    <Answer>A really cool guy named Stephen Settle.</Answer>
+    <Bold>Credit</Bold>
+    <Answer>
+      All movie data as it pertains to this app came from
+      <Space pad="10px" />
+      <Image alt="tmdb" src={Tmdb} />
+    </Answer>
+  </Container>
+);
