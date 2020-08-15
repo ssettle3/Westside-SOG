4fd2b241448a3f52b39f483bd111ea9638f8a015
Stephen Settle <ssettle3@gmail.com>
2020-08-15 13:00:44 -0400

    WIP for showing all recommendations

--- src/recommendations/RecommendedMovies.js
+++ src/recommendations/RecommendedMovies.js
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { useAsync } from "react-use";
 import styled from "styled-components/macro";
+import { Loading } from "../loading/Loading";
+import { getAllRecommendations } from "../services/parse";
+
+const Container = styled.div`
+  display: flex;
+  padding: 20px;
+`;
 
 export const RecommendedMovies = () => {
-  return <div>Coming Soon</div>;
+  const [recommendations, setRecommendations] = useState([]);
+  const [loading, setLoading] = useState([]);
+
+  useAsync(async () => {
+    const recs = await getAllRecommendations();
+    setRecommendations(recs);
+    setLoading(false);
+  }, []);
+
+  return (
+    <Container>
+      {loading && <Loading />}
+      {!loading && recommendations.length > 0 && <div>Recs!</div>}
+      {!loading && recommendations.length === 0 && (
+        <div>There are no recommendations right now! Go </div>
+      )}
+    </Container>
+  );
 };
